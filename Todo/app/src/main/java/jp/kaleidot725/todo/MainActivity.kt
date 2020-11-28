2c0f140b8ca22c2bc075f3bf1d32c3b2187233b0
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-11-28 20:35:02 +0900

    feat: add new project

---
+++ Todo/app/src/main/java/jp/kaleidot725/todo/MainActivity.kt
@@ -0,0 +1,38 @@
+package jp.kaleidot725.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import jp.kaleidot725.todo.ui.TodoTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TodoTheme {
+        Greeting("Android")
+    }
+}
